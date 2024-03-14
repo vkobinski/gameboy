8fd56fc5d4c0804cae58434899f73a39d2096e98
vkobinski <victorkobinski@outlook.com>
2024-03-13 23:03:39 -0300

    cartridge structure

---
+++ src/cartridge/mod.rs
@@ -0,0 +1,4 @@
+pub mod cartridge;
+pub mod licensee;
+pub mod c_type;
+pub mod old_licensee;
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,44 @@ mod architecture;
 mod tests;
 
 use architecture::cpu::{Reg8, Cpu};
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
 
-fn main() {
+fn main()  {
     let mut cpu = Cpu::new();
 
     cpu.bank.set_8_bit_reg(&Reg8::B, 0xFF);
     cpu.adc_a_r8(Reg8::B);
+
+    let file = File::open("./games/tetris.gb");
+
+    let mut read = match file {
+        Ok(f) => {
+            println!("{:?}", f);
+            f
+        }
+        Err(e) => {
+            eprintln!("{}", e);
+            panic!("Could not open rom.");
+        }
+    };
+
+    let mut buffer = [0; 0x14F];
+    let mut buf_reader = BufReader::new(read);
+    let readed = buf_reader.read_exact(&mut buffer).expect("Could not read Rom.");
+
+    for byte in &buffer[0x0134..0x0143] {
+        print!("{} ", *byte as char);
+
+    }
+
+    for (i, byte) in buffer.into_iter().enumerate() {
+        print!("{:02x} ", byte);
+        if i % 20 == 0 {
+            println!()
+        }
+    }
+
+
 }
