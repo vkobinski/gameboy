112badb048b4dc140fb4be9dfede7e0497a0198d
vkobinski <victorkobinski@outlook.com>
2024-03-16 16:32:42 -0300

    Cartridge without banks representation done

--- src/cartridge/cartridge.rs
+++ src/cartridge/cartridge.rs
@@ -1,3 +1,5 @@
+use num_traits::WrappingSub;
+
 use super::{header::Header, rom_size};
 
 use std::io::{BufReader, Read};
@@ -17,24 +19,36 @@ impl Cartridge {
         let rom_spec = header.rom_size.clone();
         let rom_size: usize = rom_spec.size_in_bytes();
 
-        let layout = Layout::array::<u8>(rom_size).unwrap();
-        let rom: NonNull<u8>;
-
         let mut rom: Vec<u8> = vec![0; rom_size];
 
         reader.read_exact(&mut rom).unwrap();
         
-        println!("Rom size: {:02x}", rom_size);
+        let cartridge = Self {
+            header,
+            rom
+        };
 
-        Some(
-            Self {
-                header,
-                rom
-            }
-        )
+        cartridge.global_checksum_validation();
+
+        Some(cartridge)
     }
 
-    pub fn global_checksum_validation() {
+    pub fn global_checksum_validation(&self) {
+
+        let mut sum: u16 = 0;
+        for (index, byte) in self.rom.iter().enumerate() {
+
+            // The checksum should not count the bytes of the global checksum itself.
+            if index == 0x14E || index == 0x14F {
+                continue;
+            }
+
+            sum = sum.wrapping_add(u16::from(*byte));
+        }
+
+        if sum != self.header.global_checksum {
+            panic!("Global checksum validation failed.");
+        }
 
     }
 }
