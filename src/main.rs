205c80a4bb7ee78d601f3f28bf52e48fe66513e6
vkobinski <victorkobinski@outlook.com>
2025-02-23 22:05:40 -0300

    fix ci

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,21 @@ mod architecture;
 mod cartridge;
 mod tests;
 
-use architecture::cpu::{Reg8, Cpu};
-use std::io::SeekFrom;
+use architecture::cpu::{Cpu, Reg8};
 use cartridge::header::Header;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
+use std::io::SeekFrom;
 
 use crate::cartridge::cartridge::Cartridge;
 
-fn main()  {
+fn main() {
     let mut cpu = Cpu::new();
 
     let file = File::open("./games/tetris.gb");
 
-    let  read = match file {
+    let read = match file {
         Ok(f) => f,
         Err(e) => {
             eprintln!("{}", e);
@@ -26,7 +26,9 @@ fn main()  {
 
     let mut buffer = [0; 0x15F];
     let mut buf_reader = BufReader::new(read);
-    let _ = buf_reader.read_exact(&mut buffer).expect("Could not read Rom.");
+    let _ = buf_reader
+        .read_exact(&mut buffer)
+        .expect("Could not read Rom.");
 
     buf_reader.seek(SeekFrom::Start(0)).unwrap();
 
@@ -35,5 +37,4 @@ fn main()  {
     let cartridge = Cartridge::new(header, &mut buf_reader);
 
     println!("{:?}", cartridge.unwrap().header);
-
 }
